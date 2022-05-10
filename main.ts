c2ca1c8668b7997835991a1bf9e99f6ec27bf765
Furret-png <69658166+Furret-png@users.noreply.github.com>
2022-05-10 10:40:53 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -101,31 +101,80 @@ forever(function () {
 })
 /// move snake based on direction
 forever(function () {
-
-    snakeX[24] = snakeX[23]
-    snakeX[23] = snakeX[22]
-    snakeX[22] = snakeX[21]
-    snakeX[21] = snakeX[20]
-    snakeX[20] = snakeX[19]
-    snakeX[19] = snakeX[18]
-    snakeX[18] = snakeX[17]
-    snakeX[17] = snakeX[16]
-    snakeX[16] = snakeX[15]
-    snakeX[15] = snakeX[14]
-    snakeX[14] = snakeX[13]
-    snakeX[13] = snakeX[12]
-    snakeX[12] = snakeX[11]
-    snakeX[11] = snakeX[10]
-    snakeX[10] = snakeX[9]
-    snakeX[9] = snakeX[8]
-    snakeX[8] = snakeX[7]
-    snakeX[7] = snakeX[6]
-    snakeX[6] = snakeX[5]
-    snakeX[5] = snakeX[4]
-    snakeX[4] = snakeX[3]
-    snakeX[3] = snakeX[2]
-    snakeX[2] = snakeX[1]
-    snakeX[1] = snakeX[0]
+    pause(900)
+    if (length > 23) {
+        snakeX[24] = snakeX[23]
+        snakeY[24] = snakeY[23]
+    } if (length > 22) {
+        snakeX[23] = snakeX[22]
+        snakeY[23] = snakeY[22]
+    } if (length > 21) {
+        snakeX[22] = snakeX[21]
+        snakeY[22] = snakeY[21]
+    } if (length > 20) {
+        snakeX[21] = snakeX[20]
+        snakeY[21] = snakeY[20]
+    } if (length > 19) {
+        snakeX[20] = snakeX[19]
+        snakeY[20] = snakeY[19]
+    } if (length > 18) {
+        snakeX[19] = snakeX[18]
+        snakeY[19] = snakeY[18]
+    } if (length > 17) {
+        snakeX[18] = snakeX[17]
+        snakeY[18] = snakeY[17]
+    } if (length > 16) {
+        snakeX[17] = snakeX[16]
+        snakeY[17] = snakeY[16]
+    } if (length > 15) {
+        snakeX[16] = snakeX[15]
+        snakeY[16] = snakeY[15]
+    } if (length > 14) {
+        snakeX[15] = snakeX[14]
+        snakeY[15] = snakeY[14]
+    } if (length > 13) {
+        snakeX[14] = snakeX[13]
+        snakeY[14] = snakeY[13]
+    } if (length > 12) {
+        snakeX[13] = snakeX[12]
+        snakeY[13] = snakeY[12]
+    } if (length > 11) {
+        snakeX[12] = snakeX[11]
+        snakeY[12] = snakeY[11]
+    } if (length > 10) {
+        snakeX[11] = snakeX[10]
+        snakeY[11] = snakeY[10]
+    } if (length > 9) {
+        snakeX[10] = snakeX[9]
+        snakeY[10] = snakeY[9]
+    } if (length > 8) {
+        snakeX[9] = snakeX[8]
+        snakeY[9] = snakeY[8]
+    } if (length > 7) {
+        snakeX[8] = snakeX[7]
+        snakeY[8] = snakeY[7]
+    } if (length > 6) {
+        snakeX[7] = snakeX[6]
+        snakeY[7] = snakeY[6]
+    } if (length > 5) {
+        snakeX[6] = snakeX[5]
+        snakeY[6] = snakeY[5]
+    } if (length > 4) {
+        snakeX[5] = snakeX[4]
+        snakeY[5] = snakeY[4]
+    } if (length > 3) {
+        snakeX[4] = snakeX[3]
+        snakeY[4] = snakeY[3]
+    } if (length > 2) {
+        snakeX[3] = snakeX[2]
+        snakeY[3] = snakeY[2]
+    } if (length > 1) {
+        snakeX[2] = snakeX[1]
+        snakeY[2] = snakeY[1]
+    } if (length > 0) {
+        snakeX[1] = snakeX[0]
+        snakeY[1] = snakeY[0]
+    }
     /// if snake eats apple move apple, adjust score, and make snake longer
     if (snakeX[0] == appleX && snakeY[0] == appleY) {
         score += 1
@@ -139,6 +188,7 @@ forever(function () {
 /// starting pos
 let snakeX = [1]
 let snakeY = [2]
+led.plotBrightness(snakeX[0], snakeY[0], bright)
 /// plot apple (it flashes)
 basic.forever(function () {
     led.plotBrightness(appleX, appleY, bright - 55)
@@ -161,7 +211,7 @@ input.onButtonPressed(Button.B, function () {
 })
 /// pastebin
 /// snakeX[1] = snakeX[0]
-if(length>=24) {
+if (length >= 24) {
     done = 1
-    
+
 }
